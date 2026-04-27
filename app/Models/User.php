177831382c9418bf36f9c251729b3348df343374
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-04-27 23:40:02 +0530

    Add multi-language IDs support to doctor register API

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,6 +55,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
+        'language_ids' => 'array',
     ];
     
      public function ratings()
