dcd56a146970c9973fedf53ffb3205484e153209
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-04-28 17:19:03 +0530

    Move admin table styling from inline blocks into noraya theme css

--- resources/views/admin/countries.blade.php
+++ resources/views/admin/countries.blade.php
@@ -5,21 +5,19 @@
     {{ session('success') }}
 </div>
 @endif
-<!-- Bootstrap CSS -->
+
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-<!-- DataTables CSS -->
 <link href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css" rel="stylesheet">
-<!-- jQuery and Bootstrap JS -->
+
 <div class="row">
     <div class="col-md-12">
-        <div class="card mb-4">
-            <div class="card-header">
-                <h2 class="card-title">Country
-                </h2>
-                <a href="{{ route('admin.country.add') }}" class="btn btn-primary float-end">Add Country</a>
-            </div> <!-- /.card-header -->
+        <div class="card mb-4 pending-doctors-card">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <h3 class="card-title mb-0">Countries</h3>
+                <a href="{{ route('admin.country.add') }}" class="btn btn-primary">Add Country</a>
+            </div>
             <div class="card-body">
-                <table id="datatable" class="table table-bordered">
+                <table id="datatable" class="table table-bordered table-hover pending-doctors-table">
                     <thead>
                         <tr>
                             <th style="width: 10px">#</th>
@@ -30,27 +28,22 @@
                     </thead>
                     <tbody>
                         @if(isset($countries) && count($countries) > 0)
-                        @foreach($countries as $key => $country)
+                        @foreach($countries as $country)
                         <tr class="align-middle">
                             <td>{{ $country->id }}</td>
                             <td>{{ $country->countryname }}</td>
-                            @if(!empty($country->code))
-                            @php
-                            $countryImage = config('app.url').'flags/'.strtolower($country->code).'.png';
-                            @endphp
-                            @endif
-                            {{--<td><img src="{{$countryImage }}" alt="{{ $country->countryname }}" height="50px" width="60px"/></td> --}}
                             <td>{{ $country->countrycode }}</td>
-                            <td class="text-center"><a href="{{ url('/admin/country/' . $country->id . '/edit') }}" type="button" class="btn btn-info">edit</a>
-
-
-                                <form action="{{ route('admin.country.delete', $country->id) }}" method="POST" style="display:inline;">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this Country?');">
-                                        Delete
-                                    </button>
-                                </form>
+                            <td class="text-center">
+                                <div class="action-buttons">
+                                    <a href="{{ url('/admin/country/' . $country->id . '/edit') }}" type="button" class="btn btn-outline-info">edit</a>
+                                    <form action="{{ route('admin.country.delete', $country->id) }}" method="POST" style="display:inline;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-outline-danger" onclick="return confirm('Are you sure you want to delete this Country?');">
+                                            Delete
+                                        </button>
+                                    </form>
+                                </div>
                             </td>
                         </tr>
                         @endforeach
@@ -61,33 +54,22 @@
                         @endif
                     </tbody>
                 </table>
-            </div> <!-- /.card-body -->
-            <!-- <div class="card-footer clearfix">
-                                    <ul class="pagination pagination-sm m-0 float-end">
-                                        <li class="page-item"> <a class="page-link" href="#">&laquo;</a> </li>
-                                        <li class="page-item"> <a class="page-link" href="#">1</a> </li>
-                                        <li class="page-item"> <a class="page-link" href="#">2</a> </li>
-                                        <li class="page-item"> <a class="page-link" href="#">3</a> </li>
-                                        <li class="page-item"> <a class="page-link" href="#">&raquo;</a> </li>
-                                    </ul>
-                                </div> -->
-        </div> <!-- /.card -->
-
-    </div> <!-- /.col -->
-
+            </div>
+        </div>
+    </div>
 </div>
+
 <script type="text/javascript">
     $(document).ready(function() {
-        // Initialize DataTable
         $('#datatable').DataTable({
-            "paging": true, // Enable pagination
-            "searching": true, // Enable search
-            "ordering": true, // Enable sorting
-            "info": true, // Enable info text (e.g., "Showing 1 to 10 of 50 entries")
-            "lengthChange": true, // Allow changing the number of rows per page
-            "autoWidth": false, // Disable automatic column width calculation
+            "paging": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "lengthChange": true,
+            "autoWidth": false,
             "columnDefs": [{
-                "targets": [0], // Disable sorting for the ID column (optional)
+                "targets": [0],
                 "orderable": false
             }]
         });
