8b0830598139c7318e8ea15af4dd467400513834
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-06-16 01:02:18 +0530

    Update registration approval email

--- app/Mail/SendApproval.php
+++ app/Mail/SendApproval.php
@@ -5,7 +5,6 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -47,15 +46,4 @@ class SendApproval extends Mailable implements ShouldQueue
         );
     }
 
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [
-            Attachment::fromPath(public_path('/assets/documents/issuing-prescription.pdf')), // Static PDF file
-        ];
-    }
 }
