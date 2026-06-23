dd37f856782dcfba2b1acc777bb310173a414f91
meenakshi1811 <136489830+meenakshi1811@users.noreply.github.com>
2026-06-23 19:00:56 +0530

    new changes

--- app/Mail/UserOnboardingMail.php
+++ app/Mail/UserOnboardingMail.php
@@ -3,14 +3,13 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class UserOnboardingMail extends Mailable implements ShouldQueue
+class UserOnboardingMail extends Mailable
 {
     use Queueable, SerializesModels;
 
